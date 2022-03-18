57d85c9fd12ce0ebe930c7cf9769c7012c15ff57
ptrcnull <git@ptrcnull.me>
2022-03-18 11:03:45 +0100

    fix: Make setting first name more resistant to updates

--- app/src/main/java/me/ptrcnull/revolutnamechanger/NameChanger.java
+++ app/src/main/java/me/ptrcnull/revolutnamechanger/NameChanger.java
@@ -1,13 +1,18 @@
 package me.ptrcnull.revolutnamechanger;
 
+import android.annotation.SuppressLint;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+import de.robv.android.xposed.callbacks.XCallback;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class NameChanger implements IXposedHookLoadPackage {
@@ -49,6 +54,7 @@ public class NameChanger implements IXposedHookLoadPackage {
         throw new Exception("Could not find root checker activity");
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadedPackage) throws Throwable {
         if (!Objects.equals(loadedPackage.packageName, "com.revolut.revolut")) {
@@ -62,13 +68,38 @@ public class NameChanger implements IXposedHookLoadPackage {
         Field rootCheckerActivityField = findActivityField(loginActivity.getDeclaredFields());
         XposedBridge.hookAllMethods(loginActivity, "onCreate", new RootCheckerBypass(rootCheckerActivityField));
 
-        final Class<?> userClass = XposedHelpers.findClass("sm0.b", loadedPackage.classLoader);
-        XposedBridge.hookAllConstructors(userClass, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                param.args[1] = "Name";
+        // even more massive heuristics
+        // get Retrofit service
+        Class<?> userService = loadedPackage.classLoader.loadClass("com.revolut.feature.user.impl.network.service.UserService");
+        // @GET currentUser() returns Single<UserDto>
+        Method currentUserMethod = userService.getMethod("currentUser");
+        ParameterizedType returnType = (ParameterizedType) currentUserMethod.getGenericReturnType();
+        Type userDtoType = returnType.getActualTypeArguments()[0];
+        Class<?> userDto = loadedPackage.classLoader.loadClass(userDtoType.getTypeName());
+        // UserDto has only one field with type UserInfoDto
+        Class<?> userInfoDto = userDto.getDeclaredFields()[0].getType();
+        XposedBridge.log(Arrays.toString(userInfoDto.getConstructors()));
+        Field firstNameField = null;
+        // get field marked with annotation @SerializedName("firstName")
+        for (Field dtoField : userInfoDto.getDeclaredFields()) {
+            // ugly shit
+            if (Arrays.toString(dtoField.getAnnotations()).contains("firstName")) {
+                firstNameField = dtoField;
             }
-        });
+        }
+        Field finalFirstNameField = firstNameField;
+        finalFirstNameField.setAccessible(true);
+        // bind every getter method to overwrite the firstName field
+        // with java reflection there's no way to get which field a method is accessing,
+        // so this is the best we can settle for, at least for now
+        for (Method method : userInfoDto.getDeclaredMethods()) {
+            XposedBridge.hookMethod(method, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    finalFirstNameField.set(param.thisObject, "Name");
+                }
+            });
+        }
 
         XposedBridge.log("initialized");
     }
